fdabb3df8623fab14c2515f81e1deb11ba316ca4
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-16 11:10:10 +0300

    Create SideBar.tsx

---
+++ app/components/navigation/SideBar.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom"
+
+function Sidebar() {
+  return (
+    <aside className="sidebar">
+
+      <h3>IDL</h3>
+
+      <nav>
+
+        <p>Hub</p>
+
+        <Link to="/hub/dashboard">Dashboard</Link>
+        <Link to="/hub/courses">Courses</Link>
+        <Link to="/hub/community">Community</Link>
+        <Link to="/hub/events">Events</Link>
+        <Link to="/hub/ai">Ask AI</Link>
+
+        <hr />
+
+        <p>Learning</p>
+
+        <Link to="/learn/dashboard">Dashboard</Link>
+        <Link to="/learn/courses">Courses</Link>
+        <Link to="/learn/lessons">Lessons</Link>
+        <Link to="/learn/live-classes">Live Classes</Link>
+        <Link to="/learn/scheduler">Scheduler</Link>
+
+      </nav>
+
+    </aside>
+  )
+}
+
+export default Sidebar
