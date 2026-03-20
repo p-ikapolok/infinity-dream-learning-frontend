304129c007fca76cf9110c35f801c89aed064d04
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-20 11:51:48 +0300

    Update ThemeContext.tsx

--- app/context/ThemeContext.tsx
+++ app/context/ThemeContext.tsx
@@ -1,25 +1,17 @@
 import { createContext, useState, useEffect, ReactNode } from "react"
 
-type ThemeContextType = {
+interface ThemeContextType {
   dark: boolean
-  setDark: (value: boolean) => void
+  setDark: (val: boolean) => void
 }
 
-export const ThemeContext = createContext<ThemeContextType>({
-  dark: false,
-  setDark: () => {}
-})
+export const ThemeContext = createContext<ThemeContextType | null>(null)
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-
   const [dark, setDark] = useState(false)
 
   useEffect(() => {
-    if (dark) {
-      document.documentElement.classList.add("dark")
-    } else {
-      document.documentElement.classList.remove("dark")
-    }
+    document.body.className = dark ? "dark" : ""
   }, [dark])
 
   return (
