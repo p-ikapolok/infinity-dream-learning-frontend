0637671dac3f0f9245068bcc89b8f608c876af67
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-21 01:19:19 +0300

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,9 @@
 import type { Route } from "./+types/home";
 import Welcome from "../pages/public/Welcome";
 
+// Added this line
+export { default } from "../pages/public/Home";
+
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Infinity Dream Learning" },
