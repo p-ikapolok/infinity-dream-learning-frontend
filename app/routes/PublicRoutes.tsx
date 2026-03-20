6a67d60f8d71ff69c42536c3065f0b8e2e310af9
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-20 04:30:17 +0300

    Update PublicRoutes.tsx

--- app/routes/PublicRoutes.tsx
+++ app/routes/PublicRoutes.tsx
@@ -1,10 +1,9 @@
+// src/routes/PublicRoutes.tsx
 import { Routes, Route } from "react-router-dom"
-
-import PublicLayout from "../layouts/PublicLayout"
-
+import Welcome from "../pages/public/Welcome"
 import Home from "../pages/public/Home"
-import Courses from "../pages/public/Courses"
 import About from "../pages/public/About"
+import Courses from "../pages/public/Courses"
 import Pricing from "../pages/public/Pricing"
 import Contact from "../pages/public/Contact"
 import Login from "../pages/auth/Login"
@@ -12,19 +11,16 @@ import Register from "../pages/auth/Register"
 
 function PublicRoutes() {
   return (
-    <PublicLayout>
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-
-      </Routes>
-    </PublicLayout>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/courses" element={<Courses />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
   )
 }
 
