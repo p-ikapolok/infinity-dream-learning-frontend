f2c46174ec42c7c9cc543011102195b7711c6b2e
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-20 12:15:27 +0300

    Update Courses.tsx

---
+++ app/pages/public/Courses.tsx
@@ -0,0 +1,8 @@
+// Path: src/pages/public/courses.tsx
+export default function Courses() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Courses Page</h1>
+    </div>
+  )
+}
