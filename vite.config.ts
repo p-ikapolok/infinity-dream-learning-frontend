6bb0b1005585fd19dc9f20cf260067c30f4a9fcc
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-21 01:38:45 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react(),
-    vercelPreset(), 
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths()
   ],
-});
+})
