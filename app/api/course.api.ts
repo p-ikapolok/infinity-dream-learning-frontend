ccf5472d5db7cdad494888c5a16a6d5801466196
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-20 12:18:48 +0300

    Update course.api.ts

--- app/api/course.api.ts
+++ app/api/course.api.ts
@@ -1,21 +1,7 @@
+// Path: src/api/lesson.api.ts
 import http from "./http"
 
-export const getCourses = async () => {
-  const { data } = await http.get("/courses")
-  return data
-}
-
-export const getCourseById = async (id: string) => {
-  const { data } = await http.get(`/courses/${id}`)
-  return data
-}
-
-export const enrollCourse = async (courseId: string) => {
-  const { data } = await http.post(`/courses/${courseId}/enroll`)
-  return data
-}
-
-export const getMyCourses = async () => {
-  const { data } = await http.get("/courses/my")
+export const getLessons = async () => {
+  const { data } = await http.get("/lessons")
   return data
 }
