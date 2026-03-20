9f4212f5fcb025c960809fcd4f6f24320d3d686a
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-20 12:12:40 +0300

    Update Contact.tsx

---
+++ app/pages/public/Contact.tsx
@@ -0,0 +1,8 @@
+// Path: src/pages/public/contact.tsx
+export default function Contact() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold">Contact Us</h1>
+    </div>
+  )
+}
