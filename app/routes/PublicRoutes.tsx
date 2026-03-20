374e11f6fc5a89c710a77039fa95eeabeb24861f
Peter Ikapolok <117816311+p-ikapolok@users.noreply.github.com>
2026-03-20 12:57:02 +0300

    Update PublicRoutes.tsx

--- app/routes/PublicRoutes.tsx
+++ app/routes/PublicRoutes.tsx
@@ -1,19 +1,31 @@
-// Path: src/routes/PublicRoutes.tsx
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
+
 import Welcome from "../pages/public/Welcome"
 import Home from "../pages/public/Home"
+import About from "../pages/public/About"
 import Courses from "../pages/public/Courses"
 import Pricing from "../pages/public/Pricing"
 import Contact from "../pages/public/Contact"
 
-export default function PublicRoutes() {
+function PublicRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Welcome />} />
+
+      {/* Redirect root → welcome */}
+      <Route path="/" element={<Navigate to="/welcome" />} />
+
+      {/* Welcome splash */}
+      <Route path="/welcome" element={<Welcome />} />
+
+      {/* Actual public pages */}
       <Route path="/home" element={<Home />} />
+      <Route path="/about" element={<About />} />
       <Route path="/courses" element={<Courses />} />
       <Route path="/pricing" element={<Pricing />} />
       <Route path="/contact" element={<Contact />} />
+
     </Routes>
   )
 }
+
+export default PublicRoutes
